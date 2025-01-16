e55fd7d2534c67e8fad19da3f2f118119e8d67f7
ericpicot <eric.picot@hotmail.fr>
2025-01-16 11:15:36 +0000

    Remove unused files and classes, including game-related components and reward prediction utilities; add a basic robots.txt file

--- coinche/__init__.py
+++
@@ -1,7 +0,0 @@
-from gym.envs.registration import register
-
-register(
-    id='Coinche_Game-v0',
-    entry_point='Coinche.Coinche:CoincheEnv',
-    kwargs={'playersName': ['Nord', 'Est', 'Sud', 'Ouest'], 'maxScore': 1000}
-)
